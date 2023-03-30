e251620a22eddcc30d6dfacf3fe7afc5ea5d99b7
Yoshiyuki Nakano <syxqh349ysykn@gmail.com>
2023-03-30 18:18:38 +0900

    Initial commit

---
+++ bin/line-bot-ts-sample.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { LineBotTsSampleStack } from '../lib/line-bot-ts-sample-stack';
+
+const app = new cdk.App();
+new LineBotTsSampleStack(app, 'LineBotTsSampleStack');
