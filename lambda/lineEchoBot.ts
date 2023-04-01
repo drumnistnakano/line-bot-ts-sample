1dc62252c21c75bb5b91c9925c39327d703cbea0
Yoshiyuki Nakano <syxqh349ysykn@gmail.com>
2023-04-01 10:28:47 +0900

    modify lambda proxy setup

--- lambda/lineEchoBot.ts
+++ lambda/lineEchoBot.ts
@@ -1,5 +1,16 @@
-export const handler = async (event: any): Promise<any> => {
+import * as line from '@line/bot-sdk'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+
+const config = {
+  channelAccessToken: 'YOUR_CHANNEL_ACCESS_TOKEN',
+  channelSecret: 'YOUR_CHANNEL_SECRET',
+}
+
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   console.log(event)
+  const client = new line.Client(config)
   return {
     statusCode: 200,
     body: 'Hello Wolrd!',
